84c3a4b065dff411044de8c1bc928d81aae9fc53
Harshitha Gummadi <gummadiharshitha06@gmail.com>
2025-04-11 01:49:27 +0530

    Updated API URL to Render backend

--- jobtracker/src/Components/auth.js
+++ jobtracker/src/Components/auth.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://jobtracker-cvx5.onrender.com';
 
 export const register = async (email, password) => {
   try {
